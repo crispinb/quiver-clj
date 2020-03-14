2e17473c943f8bad8121bde5df01c3371ce11aee
Crispin Bennett <myself@crisbennett.com>
2020-03-14 21:11:17 +1100

    Another magitty test commit

--- src/quiver/core.clj
+++ src/quiver/core.clj
@@ -2,10 +2,11 @@
 
 (defn get-json [filename]
   (->
-    filename
-    ;; best way to test for existence of the resource?
+       ;; best way to test for existence of the resource?
 ;; and perhaps validate the json?
     io/resource
     slurp))
 
 (get-json "test-file.json")
+
+(.toUpperCase to"fark")
