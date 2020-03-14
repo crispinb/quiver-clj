30445d392c37f0cada9310f8eb884f51df49d779
Crispin Bennett <myself@crisbennett.com>
2020-03-15 10:55:11 +1100

    Mo stagey

--- src/quiver/core.clj
+++ src/quiver/core.clj
@@ -1,4 +1,4 @@
-(ns quiver.core (:require [clojure.java.io :as io]))
+(ns quiver.core (:require [clojure.java.io :as io]))q
 
 (defn get-json [filename]
   (->
@@ -10,4 +10,3 @@
 
 (get-json "test-file.json")
 (get-json "test-data2.json")
-
