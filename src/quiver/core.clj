09fcd2353337b5cdc43d778da7121780ba16f566
Crispin Bennett <myself@crisbennett.com>
2020-03-26 11:49:20 +1100

    Basic (very) list notes functioning

--- src/quiver/core.clj
+++ src/quiver/core.clj
@@ -1,12 +1,24 @@
-(ns quiver.core (:require [clojure.java.io :as io]))q
+(ns quiver.core (:require
+                 [clojure.java.io :as io]
+                 [clojure.string :as str]
+                 [clojure.data.json :as json]))
 
-(defn get-json [filename]
+(defn get-file-contents [filename]
   (->
    filename
-   ;; best way to test for existence of the resource?
-   ;; and perhaps validate the json?
-   io/resource
    slurp))
 
-(get-json "test-file.json")
-(get-json "test-data2.json")
+(def valid-notebook "test-data/Valid_notebook_with_two_notes.qvnotebook/")
+
+(defn is-note? [name]
+  (str/ends-with? name ".qvnote"))
+
+(defn seq-note-paths [path]
+  (map #(str (.getAbsolutePath %1) "/meta.json")
+       (filter #(is-note? (.getName %1)) (file-seq (io/file path)))))
+
+
+(defn seq-note-metadata [note-paths]
+  (map json/read-str (map #(get-file-contents %1) note-paths)))
+
+(seq-note-metadata (seq-note-paths valid-notebook))
