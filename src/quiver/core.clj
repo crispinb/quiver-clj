7ce4ac6c9048597cf06f8f81659cad95ef5ead69
Crispin Bennett <myself@crisbennett.com>
2020-03-30 16:05:36 +1100

    Add basic cmdline parsing to pass a notebook path
    but
    - no error if it's wrong
    - crude output

--- src/quiver/core.clj
+++ src/quiver/core.clj
@@ -1,12 +1,22 @@
+;; TODO: cmd to get list of note titles
+;; TODO; cmdline help/options for   that cmd
+;; TODO: return an error if the path's not a notebook
+;; TODO: cmd to get note content for a given tag
+;; TODO: tests (as per rust verion) (look into that test library, and that deps.edn post)
+;; TODO: create an uberjar and run from java??
+
 (ns quiver.core
   (:require [quiver.lib :as lib])
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
 (def valid-notebook "test-data/Valid_notebook_with_two_notes.qvnotebook/")
 
-;; accept notebook fiel path argument  https://github.com/clojure/tools.cli
-
+(def cmdline-options [["-d" "--notebookdir Notebook directory"]
+                      ["-h" "--help"]])
 
 (defn -main [& args]
-  (println (lib/seq-note-metadata (lib/seq-note-paths valid-notebook)))
+  (def opts (parse-opts args cmdline-options))
+  (def titles (lib/get-note-titles (get-in opts [:options :notebookdir])))
+  (println (str "your titles Sir: \n" (pr-str titles)))
   )
--- src/quiver/lib.clj
+++ src/quiver/lib.clj
@@ -19,5 +19,14 @@
 (defn seq-note-metadata [note-paths]
   (map json/read-str (map #(get-file-contents %1) note-paths)))
 
+(defn get-note-titles [notebook-path]
+  (let [notes (seq-note-metadata (seq-note-paths notebook-path))]
+    (map #(get %1"title") notes)))
+
 (comment
+  (def valid-notebook "test-data/Valid_notebook_with_two_notes.qvnotebook/")
+  valid-notebook
+  (get-note-titles valid-notebook)
+  (map :x '({:x "thing" :y "thang"} {:x "sauage"}))
+  (get {"x" "sausage"} "x")
   (seq-note-metadata (seq-note-paths valid-notebook)))
