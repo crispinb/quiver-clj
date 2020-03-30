9731bf03afe82516c324f61e825dccef22a9da54
Crispin Bennett <myself@crisbennett.com>
2020-03-30 16:39:43 +1100

    Add user feedback if no notes found

--- src/quiver/core.clj
+++ src/quiver/core.clj
@@ -1,6 +1,3 @@
-;; TODO: cmd to get list of note titles
-;; TODO; cmdline help/options for   that cmd
-;; TODO: return an error if the path's not a notebook
 ;; TODO: cmd to get note content for a given tag
 ;; TODO: tests (as per rust verion) (look into that test library, and that deps.edn post)
 ;; TODO: create an uberjar and run from java??
@@ -16,7 +13,8 @@
                       ["-h" "--help"]])
 
 (defn -main [& args]
-  (def opts (parse-opts args cmdline-options))
-  (def titles (lib/get-note-titles (get-in opts [:options :notebookdir])))
-  (println (str "your titles Sir: \n" (pr-str titles)))
-  )
+  (let [opts (parse-opts args cmdline-options)
+        titles (lib/get-note-titles (get-in opts [:options :notebookdir]))]
+    (if (seq titles)
+      (println (str "Your titles Sir: \n" (pr-str titles)))
+      (println (str "No notes found at that location")))))
