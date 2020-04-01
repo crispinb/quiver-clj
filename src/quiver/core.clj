5c723228b1932b948202c4dd5d2e039da692c902
Crispin Bennett <myself@crisbennett.com>
2020-04-01 13:00:28 +1100

    Refactoring

--- src/quiver/core.clj
+++ src/quiver/core.clj
@@ -1,4 +1,6 @@
 ;; TODO: cmd to get note content for a given tag
+;; BUG: np crash with no filename (main/titles is null)
+;; TODO: add help (does tools.cli offer anything here?)
 ;; TODO: tests (as per rust verion) (look into that test library, and that deps.edn post)
 ;; TODO: create an uberjar and run from java??
 
@@ -7,14 +9,19 @@
   (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(def valid-notebook "test-data/Valid_notebook_with_two_notes.qvnotebook/")
 
 (def cmdline-options [["-d" "--notebookdir Notebook directory"]
                       ["-h" "--help"]])
 
 (defn -main [& args]
   (let [opts (parse-opts args cmdline-options)
-        titles (lib/get-note-titles (get-in opts [:options :notebookdir]))]
+        titles (lib/note-titles (get-in opts [:options :notebookdir]))]
     (if (seq titles)
       (println (str "Your titles Sir: \n" (pr-str titles)))
       (println (str "No notes found at that location")))))
+
+(comment
+  (let [ valid-notebook "test-data/Valid_notebook_with_two_notes.qvnotebook/"
+        args (str "-f " valid-notebook)]
+    args
+    (-main args)))
--- src/quiver/lib.clj
+++ src/quiver/lib.clj
@@ -1,7 +1,7 @@
 (ns quiver.lib (:require
-                 [clojure.java.io :as io]
-                 [clojure.string :as str]
-                 [clojure.data.json :as json]))
+                [clojure.java.io :as io]
+                [clojure.string :as str]
+                [clojure.data.json :as json]))
 
 (defn get-file-contents [filename]
   (->
@@ -11,22 +11,33 @@
 (defn is-note? [name]
   (str/ends-with? name ".qvnote"))
 
-(defn seq-note-paths [path]
-  (map #(str (.getAbsolutePath %1) "/meta.json")
-       (filter #(is-note? (.getName %1)) (file-seq (io/file path)))))
+(defn note-paths [path]
+  (filter #(is-note? (.getName %1)) (file-seq (io/file path))))
 
+;; TODO look for java path append func
+(defn append-to-path [segment path]
+  (str (.getAbsolutePath path) "/" segment))
 
-(defn seq-note-metadata [note-paths]
-  (map json/read-str (map #(get-file-contents %1) note-paths)))
+(defn note-content-paths [path]
+  (map  (partial append-to-path "/content.json") (note-paths path)))
 
-(defn get-note-titles [notebook-path]
-  (let [notes (seq-note-metadata (seq-note-paths notebook-path))]
-    (map #(get %1"title") notes)))
+(defn note-metadata-paths [path]
+  (map (partial append-to-path "/meta.json") (note-paths path)))
+
+(defn note-metadata [note-metadata-paths]
+  (map json/read-str (map #(get-file-contents %1) note-metadata-paths)))
+
+(defn note-titles [notebook-path]
+  (let [notes (note-metadata (note-metadata-paths notebook-path))]
+    (map #(get %1 "title") notes)))
+
+(defn note-contents [notebook-path]
+  ;; TODO
+  )
 
 (comment
   (def valid-notebook "test-data/Valid_notebook_with_two_notes.qvnotebook/")
   valid-notebook
-  (get-note-titles valid-notebook)
-  (map :x '({:x "thing" :y "thang"} {:x "sauage"}))
-  (get {"x" "sausage"} "x")
-  (seq-note-metadata (seq-note-paths valid-notebook)))
+  (note-titles valid-notebook)
+  (note-content-paths valid-notebook)
+  (println (note-metadata (note-metadata-paths valid-notebook))))
