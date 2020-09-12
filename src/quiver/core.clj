6578d93f419f1274d4804a7defeb121e28e1a450
Crispin Bennett <myself@crisbennett.com>
2020-09-12 17:58:27 +1000

    Clearn up core.clj

--- src/quiver/core.clj
+++ src/quiver/core.clj
@@ -1,31 +1,22 @@
-;; TODO: compile base list of features for project kanban
-;; TODO: cmd to get note content for a given tag
-;; BUG: np crash with no filename (main/titles is null)
-;; TODO: add help (does tools.cli offer anything here?)
-;; TODO: tests (as per rust verion) (look into that test library, and that deps.edn post)
-;; TODO: create an uberjar and run from java??
-;; TODO: babashka for cmdline version?
-;; TODO: start with tests for library once have a real plan outnutted
-
 (ns quiver.core
   (:require [quiver.lib :as lib])
   (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
 
 (def cmdline-options [["-d" "--notebookdir Notebook directory"]
                       ["-h" "--help"]])
 
-;; (defn -main [& args]
-;;   (println "received args " args)
-;;   (let [opts (parse-opts args cmdline-options)]
-;;     (println "opts: " opts)
-;;     (lib/note-titles (get-in opts [:options :notebookdir]))))
-    ; (if (seq titles)
-    ;   (println (str "Your titles Sir: \n" (pr-str titles)))
-    ;   (println (str "No notes found at that location")))))
+(defn -main [& args]
+  (println "received args " args)
+  (let [opts (parse-opts args cmdline-options)]
+    (println "opts: " opts)
+    (lib/load-notes (get-in opts [:options :notebookdir]))))
 
 (comment
-  (def valid-n "test-data/Valid_notebook_with_two_notes.qvnotebook/")
-  valid-n
-  (-main "-d" valid-n))
+  (def valid-n (io/resource "Valid_notebook_with_two_notes.qvnotebook/"))
+  (def big-n "/mnt/c/Users/Crispin Bennett/cbfiles/archive/notes/Quiver - original format/cbnotes.qvlibrary/6F4825BD-0088-4CE3-8B29-FC8112EE0F88.qvnotebook")
+  (-main "-d" valid-n)
+  (def r (-main "-d" big-n))
+  (println (count r) "first: " (nth r 110)))
