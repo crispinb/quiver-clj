7465f947294c78d77bda9078762e4d9e053bb687
Crispin Bennett <myself@crisbennett.com>
2020-09-15 13:13:59 +1000

    Refactoring

--- src/quiver/checkvist.clj
+++ src/quiver/checkvist.clj
@@ -8,64 +8,63 @@
 (def get-token-url-seg "auth/login.json?version=2")
 (def checklists-url-seg "checklists")
 (def tasks-url-seg "tasks.json")
-;; checkvist API is very unreliable, so we rely on a small number of quick retries
+(def import-url-seg "import.json")
+
+;; TODO: store data of last store/refresh
+;; refresh if > 1day < 90 days
+;; refetch if > 90 days
+(defonce creds (atom nil))
+
+;; checkvist API is very unreliable, so we rely on repeated quick retries
 (def connection-defaults {:accept :json
                           :cookie-policy :none
                           :conn-timeout 5000
                           :headers {"X-Client-Token" @creds}
                           :content-type :json
                           :debug true :debug-body true
-                          :retry-handler (fn [ex try-count http-context]
-                                           (println "retrying connection ..." )
-                                           (< try-count 5))})
-(defonce creds (atom nil))
+                          :retry-handler (fn [_ try-count _]
+                                           (println "retrying connection ...")
+                                           (< try-count 10))})
 
-;; TODO: checkvist api times out a lot. Strategy needed (does clj-http have anything useful like retries?)
-(defn- get-auth-token [username key]
-  (-> (client/get (str (uri/join base-url get-token-url-seg))
-                  {:accept :json
-                   :cookie-policy :none
-                   ;; checkvist API can be very slow
-                   :conn-timeout 30000
-                   :query-params {"username" username "remote_key" key}})
+(defn- build-api-url [& segs]
+  (str (assoc (uri/uri base-url)
+              :path (str/join "/" (cons "" segs)))))
+
+(defn- set-auth-token
+  "Puts auth token in creds atom. Returns nil to avoid leaking the token"
+  [username key]
+  (-> (client/get  (build-api-url get-token-url-seg)
+                   (merge connection-defaults {:query-params {"username" username "remote_key" key}}))
       (:body)
       (json/read-str)
-      (get "token")))
+      (get "token")
+      (#(reset! creds %)))
+  nil)
 
 ;; don't really need this - it's just the simplest call to test the checkvist API with
 (defn- get-list-items [list-id]
-  (let [url (str (assoc (uri/uri base-url)
-                        :path (str/join "/" [nil checklists-url-seg, list-id, tasks-url-seg])))]
-    (println "calling " url)
+  (let [url (build-api-url checklists-url-seg list-id tasks-url-seg)]
     (client/get url
-                {:accept :json
-                 :cookie-policy :none
-                 :headers {"X-Client-Token" @creds}})))
+                connection-defaults)))
 
 (defn- create-task [list-id task-data]
-  (let [url (str (assoc (uri/uri base-url)
-                        :path (str/join "/"  [nil checklists-url-seg list-id tasks-url-seg])))]
-    (println "calling " url)
+  (let [url (build-api-url checklists-url-seg list-id tasks-url-seg)]
     (client/post url (merge connection-defaults {:body (json/write-str task-data)}))))
 
-
 ;; import hierarchy of tasks
-(defn- import-tasks [list-id tasks])
-
-;; TODO: is it possible it's just the checkvist api? First call from Postman took 20s; about 500ms thereafter
-;; I also just had it time out at the console
-;; maybe the checkvist calls & the maven thing are entirely separate issues
-;; 
-;; Is there anything in the google groups (I think that's their support channel for the API) about this?
+(defn- import-tasks [list-id tasks-data]
+  (let [url (build-api-url  checklists-url-seg list-id import-url-seg)]
+    (client/post url
+                 (merge connection-defaults
+                        {:body (json/write-str (merge
+                                               ;; TODO: add import options here for lines, attachments etc
+                                                {}
+                                                {:import_content tasks-data}))}))))
 
 (comment
-  (def pc-url "https://checkvist.com/p/GAjsxexcncCF74pqR6aPZq/tasks.json")
-  (def g (client/get pc-url))
-  (client/get "https://checkvist.com/checklists/774394/auth/login.json?version=2")
-  (client/get "https://checkvist.com/auth/login.json?version=2&username=myself@crisbennett.com&remote_key=k9LPfkvwnjbIOY")
-
-  (reset! creds (get-auth-token  "myself@crisbennett.com" "k9LPfkvwnjbIOY"))
   (get-list-items "774394")
   (def task-data {"content" "tested from clojure" "tags" {"#dev" false} "tags_as_text" "dev"})
+  (def import-data "Top-level-item\n  sub item 1/1\n  sub item 1/2\n    sub item 2/1")
   (create-task "774394" task-data)
+  (import-tasks "774394" import-data)
   (json/write-str task-data))
